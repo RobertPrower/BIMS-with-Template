45a2fe745a89bce20f6662a7b1a590ded13363d6
RobertPrower <robertsalas.1610@yahoo.com>
2024-03-14 04:13:56 +0800

    Revert "Naibalik yung header sa createnewresident.php"
    This reverts commit b973c751ec02863944c6e8995a4ff55ab1c00316.

--- createnewresident.php
+++ createnewresident.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>BIMS</title>
+  <title>Elegant Dashboard | Dashboard</title>
   <!-- Favicon -->
   <link rel="shortcut icon" href="./img/svg/logo.svg" type="image/x-icon">
   <!-- Custom styles -->
@@ -23,66 +23,62 @@
   <!-- ! Sidebar -->
 
   <?php
-
   include ("includes/sidebar.php");
-
   ?>
 
   <div class="main-wrapper">
-      <?php
-
-      include ("includes/header.php");
-
-      ?>
-    <div class="container">
+    <!-- ! Main nav -->
+    <?php require_once("includes/header.php")?>
+    <!-- ! Main -->
+    <main class="main users chart-page" id="skip-target">
+     <div class="container">
+        
+        <div class="container">
     <form action="include/addresidentbtn.php" method="POST" enctype="multipart/form-data">
-        <div class="form first" style="height: 850px">
+        <div class="form first">
             <div class="row">
-                <span class="title"><h2>Create New Resident</h2></span>
+                <span class="title"><h3>Create New Resident</h3></span>
 
-                <div class="card col-md-9 p-4 mt-4 " style="border-radius: 20px;">
+                <div class="card col-md-9 p-3 mt-3 " style="border-radius: 15px;">
                     <div class=" text-center row">
 
-                          <div class="form-floating mt-4 mb-3 col-md-4">
-                            <input type="text" class="form-control" id="floatingInput">
+                          <div class="form-floating mt-3 mb-3 col-md-4">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
                             <label for="floatingInput">First Name</label>
                           </div>
 
-                          <div class="form-floating mt-4 mb-3 col-md-4">
-                            <input type="text" class="form-control" id="floatingInput">
+                          <div class="form-floating mt-3 mb-3 col-md-4">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
                             <label for="floatingInput">Middle Name</label>
                           </div>
 
-                          <div class="form-floating mt-4 mb-3 col-md-4">
-                            <input type="text" class="form-control" id="floatingInput">
+                          <div class="form-floating mt-3 mb-3 col-md-4">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
                             <label for="floatingInput">Last Name</label>
                           </div>
 
-                          <div class="form-floating mt-4 mb-3 col-md-4">
-                            <input type="text" class="form-control" id="floatingInput" >
+                          <div class="form-floating mt-3 mb-3 col-md-4">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
                             <label for="floatingInput">House No. (Blk no, Lot no, Unit no)</label>
                           </div>
 
-                          <div class="form-floating mt-4 mb-3 col-md-4">
-                            <input type="text" class="form-control" id="floatingInput" >
+                          <div class="form-floating mt-3 mb-3 col-md-4">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
                             <label for="floatingInput">Street</label>
                           </div>
 
-                          <div class="form-floating mt-4 mb-3 col-md-4">
-                            <input type="text" class="form-control" id="floatingInput" >
+                          <div class="form-floating mt-3 mb-3 col-md-4">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
                             <label for="floatingInput">Subdivision</label>
                           </div>
 
-                          <div class="mt-3 mb-3 col-md-4">
-                            <select class="form-select" aria-label="Default select example" Style="Height: 58px">
-                              <option hidden selected>Sex</option>
-                              <option value="Male">Male</option>
-                              <option value="Female">Female</option>
-                            </select>
+                          <div class="form-floating mt-3 mb-3 col-md-4">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
+                            <label for="floatingInput">Sex</label>
                           </div>
 
                           <div class="mt-3 mb-3 col-md-4">
-                            <select class="form-select" aria-label="Default select example" Style="Height: 58px">
+                            <select class="form-select" aria-label="Default select example" Style="Height: 48px">
                               <option hidden selected>Select Marital Status</option>
                               <option value="Single">Single</option>
                               <option value="Married">Married</option>
@@ -93,33 +89,36 @@
                           </div>
 
                           <div class="form-floating mt-3 mb-3 col-md-4">
-                            <input type="Date" class="form-control" id="floatingInput">
+                            <input type="Date" class="form-control" id="floatingInput" placeholder="name@example.com">
                             <label for="floatingInput">Birth Date</label>
                           </div> 
 
                           <div class="form-floating mt-3 mb-3 col-md-4">
-                            <input type="Text" class="form-control" id="floatingInput">
+                            <input type="Text" class="form-control" id="floatingInput" placeholder="name@example.com">
                             <label for="floatingInput">Birth Place</label>
                           </div> 
 
                           <div class="form-floating mt-3 mb-3 col-md-4">
-                            <input type="" class="form-control" id="floatingInput">
+                            <input type="" class="form-control" id="floatingInput" placeholder="name@example.com">
                             <label for="floatingInput">Cellphone Number</label>
                           </div> 
 
                           <div class="mt-3 mb-3 col-md-4">
-                            <select class="form-select" aria-label="Default select example" Style="Height: 58px">
+                            <select class="form-select" aria-label="Default select example" Style="Height: 48px">
                               <option hidden selected>Is a Voter?</option>
                               <option value="Yes">Yes</option>
                               <option value="No">No</option>
                             </select>
-                          </div>     
+                          </div>
+                    
+
+
                     </div>
                 </div>
 
-                <div class="justify-content-md-end col-md-3 mt-3">
+                <div class="justify-content-md-end col-md-3 mt-3 row-md-3">
 
-                  <div class="card" style="border-radius: 15px;">
+                  <div class="card" style="border-radius: 15px; height: 310px">
                       <div class="card-body text-center">
                           <div class="mt-3 mb-4">
                               <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
